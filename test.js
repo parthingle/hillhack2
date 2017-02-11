7ec3e66c0d5d928d8eb168ac881ffc4935a0890a
il-dionigi <brian.dionigi@gmail.com>
2017-02-11 11:45:17 -0800

    senator names
    Contains first half of senator names and link to opensecrets

---
+++ test.js
@@ -0,0 +1,59 @@
+test
+
+class senator{
+	constructor(name, web)
+}
+
+class represenative{
+	constructor(name, web)
+}
+
+var rabraham=new representative("Ralph Abraham", "https://www.opensecrets.org/politicians/summary.php?cid=N00036633&cycle=2016)";
+var srshelby=new senator("Richard Shelby", "https://www.opensecrets.org/politicians/summary.php?cid=N00009920&cycle=2016");
+var slstrange=new senator("Luther Strange", "https://www.opensecrets.org/lobby/lobbyist.php?id=Y0000025847L&year=2007");
+var slmurkowski=new senator("Lisa Murkowski", "https://www.opensecrets.org/politicians/summary.php?cid=N00026050&cycle=2016");
+var sdsullivan=new senator("Dan Sullivan", "https://www.opensecrets.org/politicians/summary.php?cid=N00035774&cycle=2016");
+var sjmccain=new senator("John McCain", "https://www.opensecrets.org/politicians/summary.php?cid=N00006424&cycle=2016");
+var sjflake=new senator("Jeff Flake", "https://www.opensecrets.org/politicians/summary.php?cid=N00009573&cycle=2016");
+var sjboozman=new senator("John Boozman", "https://www.opensecrets.org/politicians/summary.php?cid=N00013873&cycle=2016");
+var stcotton=new senator("Tom Cotton", "https://www.opensecrets.org/politicians/summary.php?cid=N00033363&cycle=2016");
+var sdfeinstein=new senator("Dianne Feinstein", "https://www.opensecrets.org/politicians/summary.php?cid=N00007364&cycle=2016");
+var skharris=new senator("Katherine Harris", "https://www.opensecrets.org/politicians/summary.php?cid=N00024882&cycle=2006");
+var smbennet=new senator("Michael Bennet", "https://www.opensecrets.org/politicians/summary.php?cid=N00030608&cycle=2016");
+var scgardner=new senator("Cory Gardnet", "https://www.opensecrets.org/politicians/summary.php?cid=N00030780&cycle=2016");
+var srblumenthal=new senator("Richard Blumenthal"; "https://www.opensecrets.org/politicians/summary.php?cid=N00031685&cycle=2016");
+var scmurphy=new senator("Christopher Murphy", "https://www.opensecrets.org/politicians/summary.php?cid=N00027566&cycle=2016");
+var stcarper=new senator("Tom Carper", "https://www.opensecrets.org/politicians/summary.php?cid=N00012508&cycle=2016");
+var sccoons=new senator("Chris Coons", "https://www.opensecrets.org/politicians/summary.php?cid=N00031820&cycle=2016");
+var sbnelson=new senator("Bill Nelson", "https://www.opensecrets.org/politicians/summary.php?cid=N00009926&cycle=2016");
+var smrubio=new senator("Marco Rubio", "https://www.opensecrets.org/politicians/summary.php?cid=N00030612&cycle=2016");
+var sjisakson=new senator("Johnny Isakson", "https://www.opensecrets.org/politicians/summary.php?cid=N00002593&cycle=2016");
+var sdperdue=new senator("David Perdue", "https://www.opensecrets.org/politicians/summary.php?cid=N00035516&cycle=2016");
+var sbschatz=new senator("Brian Schatz", "https://www.opensecrets.org/politicians/summary.php?cid=N00028138&cycle=2016");
+var smhirono=new senator("Mazie Hirono", "https://www.opensecrets.org/politicians/summary.php?cid=N00028139&cycle=2016");
+var smcrapo=new senator("Mike Crapo", "https://www.opensecrets.org/politicians/summary.php?cid=N00006267&cycle=2016");
+var sjrisch=new senator("James Risch", "https://www.opensecrets.org/politicians/summary.php?cid=N00029441&cycle=2016");
+var sddurbin=new senator("Dick Durbin", "https://www.opensecrets.org/politicians/summary.php?cid=N00004981&cycle=2016");
+var stduckworth=new senator("Tammy Duckworth", "https://www.opensecrets.org/politicians/summary.php?cid=N00027860&cycle=2016");
+var sjdonnelly=new senator("Joe Donnelly", "https://www.opensecrets.org/politicians/summary.php?cid=N00026586&cycle=2016");
+var styoung=new senator("Todd Young", "https://www.opensecrets.org/politicians/summary.php?cid=N00030670&cycle=2016");
+var scgrassley=new senator("Chuck Grassley", "https://www.opensecrets.org/politicians/summary.php?cid=N00001758&cycle=2016");
+var sjernst=new senator("Joni Ernst", "https://www.opensecrets.org/politicians/summary.php?cid=N00035483&cycle=2016");
+var sproberts=new senator("Pat Roberts", "https://www.opensecrets.org/politicians/summary.php?cid=N00005285&cycle=2016");
+var sjmoran=new senator("Jerry Moran", "https://www.opensecrets.org/politicians/summary.php?cid=N00005282&cycle=2016");
+var smmcconnell=new senator("Mitch McConnell", "https://www.opensecrets.org/politicians/summary.php?cid=N00003389&cycle=2016");
+var srpaul=new senator("Rand Paul", "https://www.opensecrets.org/politicians/summary.php?cid=N00030836&cycle=2016");
+var sbcassidy=new senator("Bill Cassidy", "https://www.opensecrets.org/politicians/summary.php?cid=N00030245&cycle=2016");
+var sjkennedy=new senator("John Kennedy", "NA");
+var sscollins=new senator("Susan Collins", "https://www.opensecrets.org/politicians/summary.php?cid=N00000491&cycle=2016");
+var saking=new senator("Angus King", "https://www.opensecrets.org/politicians/summary.php?cid=N00034580&cycle=2016");
+var sbcardin=new senator("Ben Cardin", "https://www.opensecrets.org/politicians/summary.php?cid=N00001955&cycle=2016");
+var scvhollen=new senator("Chris Van Hollen", "https://www.opensecrets.org/politicians/summary.php?cid=N00013820&cycle=2016");
+var sewarren=new senator("Elizabeth Warren", "https://www.opensecrets.org/politicians/summary.php?cid=N00033492&cycle=2016");
+var semarkey=new senator("Ed Markey", "https://www.opensecrets.org/politicians/summary.php?cid=N00000270&cycle=2016");
+var sdstabenow=new senator("Debbie Stabenow", "https://www.opensecrets.org/politicians/summary.php?cid=N00004118&cycle=2016");
+var sgpeters=new senator("Gary Peters", "https://www.opensecrets.org/politicians/summary.php?cid=N00029277&cycle=2016");
+var saklobuchar=new senator("Amy Klobuchar", "https://www.opensecrets.org/politicians/summary.php?cid=N00027500&cycle=2016");
+var safranken=new senator("Al Franken", "https://www.opensecrets.org/politicians/summary.php?cid=N00029016&cycle=2016");
+var stcochran=new senator("Thad Chochran", "https://www.opensecrets.org/politicians/summary.php?cid=N00003328&cycle=2016");
+var srwicker=new senator("Roger Wicker", "https://www.opensecrets.org/politicians/summary.php?cid=N00003280&cycle=2016");
